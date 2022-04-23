e98a9be9987a2440982b77d7971eff3ecaa089ef
lnardini <llnardini1@gmail.com>
2022-04-23 15:01:46 -0400

    Progress on main app, updated TODO list

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,38 +1,84 @@
 package com.company;
 
-import com.mysql.cj.x.protobuf.MysqlxPrepare;
-
 import java.sql.*;
+import java.sql.Date;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     static final String URL = "";
-    static String user = "";
-    static String pass = "";
+    static String mySqlUser = "";
+    static String mySqlPass = "";
     static Scanner input = new Scanner(System.in);
     static final String USAGE = " ";
+
+
+    static String accountUsername = "";
+    static String accountPassword = "";
 
     // Argument String constants
     static final String ALLGENRE = "--allGenre";
     static final String ANYGENRE = "--anyGenre";
     static final String AUTHOR = "--author";
     static final String SORT = "--sort";
+    static final Date currentDate = new Date(System.currentTimeMillis());
 
     static final List<String> FLAGS = Arrays.asList(ALLGENRE, ANYGENRE, AUTHOR, SORT);
 
 
     public static void main(String[] args) {
         System.out.println("Enter the MySql username");
-        user = input.next();
+        mySqlUser = input.next();
         System.out.println("Enter the MySql password");
-        pass = input.next();
+        mySqlPass = input.next();
 
 
-        try(Connection conn = DriverManager.getConnection(URL, user, pass);
+        try(Connection conn = DriverManager.getConnection(URL, mySqlUser, mySqlPass);
             Statement stmt = conn.createStatement()) {
             System.out.println("Connection successful!");
+
+            // Account
+            ResultSet accounts = stmt.executeQuery("SELECT * FROM musicReview.reviewerUser;");
+            List<String[]> accountInfo = new ArrayList<>(); // [username, userPassword, email]
+            while (accounts.next()) {
+                accountInfo.add(new String[]{
+                        accounts.getString("username"),
+                        accounts.getString("userPassword"),
+                        accounts.getString("email"),
+                        accounts.getString("name")
+                });
+            }
+
+            System.out.println("Would you like to create a new account? (y/n) If not, you will be prompted to sign in with an existing one.");
+            if (input.next().equalsIgnoreCase("y")) {
+                String[] newAccount = createAccount(accountInfo);
+                stmt.executeQuery(String.format("INSERT INTO reviewerUser VALUES(%s, %s, %s, %s, %s);",
+                        newAccount[0],
+                        newAccount[1],
+                        newAccount[2],
+                        currentDate,
+                        newAccount[3]));
+                accountInfo.add(newAccount);
+            }
+
+            // logging in
+            boolean loggedIn = false;
+            while (!loggedIn) {
+                System.out.println("Enter the MySql username");
+                accountUsername = input.next();
+                System.out.println("Enter the MySql password");
+                accountPassword = input.next();
+
+                for (String[] account : accountInfo) {
+                    if (account[0].equals(accountUsername) && account[1].equals(accountPassword)) {
+                        loggedIn = true;
+                        break;
+                    }
+                }
+            }
+            System.out.println("Logged in successfully");
+
 
 
             // Beginning loop
@@ -59,6 +105,15 @@ public class Main {
     }
 
     /**
+     * Creates an account for the provided user, adding it to the provided list of accounts
+     * @param accountInfo the information of the other accounts (to ensure no repeated usernames or emails)
+     */
+    static String[] createAccount(List<String[]> accountInfo) {
+        return null;
+        // TODO: Prompt user for username, password, email, and full name. Ensure username and email are unique. Return array of these items
+    }
+
+    /**
      * Takes in a review command and prompts the user for review input
      * @param command
      * @return The SQL command for adding a review
@@ -66,16 +121,28 @@ public class Main {
     static String createReviewCommand(String command) {
         List<String> argList = Stream.of(command.split(" ")).collect(Collectors.toList());
         String reviewStr = "INSERT INTO %s VALUES (%f, %s, %s, %t, %d)";
+        String entityType = argList.get(1);
+        String table = entityType.toLowerCase().concat("Review");
+        String entityName = argList.get(2);
 
+        System.out.println("What would you rate " + entityName + "?");
+        double stars = 0.0;
+        do {
+            System.out.println("Enter the number of stars between 0.5 to 5");
+            stars = input.nextDouble();
+        } while (stars > 5.0 || stars < 0.5);
 
-        return reviewStr;
+        System.out.println("Please enter a brief (<256 char) description of the " + entityType + " or press 'Enter' to skip.");
+        String description = input.nextLine();
+        String reviewer = accountUsername;
+        int entityId = -1;//TODO
+        return String.format(reviewStr, table, stars, description, reviewer, currentDate, entityId);
     }
 
 
     static Map<String, List<String>> parseCommand(String command) {
         Map<String, List<String>> args = new HashMap<>();
         List<String> argList = Stream.of(command.split(" ")).collect(Collectors.toList());
-        // TODO: Parse arguments into usable map
         String baseCmd = argList.get(0);
         args.put("COMMAND", Arrays.asList(baseCmd));
         // parsing options
